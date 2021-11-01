78aac3bc8d03939ded7263daefd79f8377cfdbc0
Anuj Kaushik <anujkaushik1512@gmail.com>
2021-11-01 12:54:04 +0530

    Implemented Parameters and Queries

--- Node JS/method.js
+++ Node JS/method.js
@@ -6,10 +6,25 @@ app.use(express.json());
 
 app.listen(3000);
 
-let users = {};
+let users = [
+    {
+        id : "1",
+        name : "Anuj kaushik",
+    },
+    {
+        id : "2",
+        name : "Nishant Jain"
+    },
+    {
+        id : "3",
+        name : "Abhishek"
+    }
+];
+
+    //Queries => 
 
 app.get("/users",(req,res) => {
-
+    console.log(req.query)
     res.send(users);
 
 });
@@ -50,3 +65,14 @@ app.delete("/users",(req,res) => {
     });
 
 });
+
+
+        //Parameters =>
+app.get("/users/:username",(req,res) =>{
+
+    res.send("user id received ");
+    console.log(req.params);   
+    console.log(req.params.username); 
+
+});
+
