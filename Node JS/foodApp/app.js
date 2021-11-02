8f019d067ab33d706e639344c5e45f914b45ecb6
Anuj Kaushik <anujkaushik1512@gmail.com>
2021-11-02 12:21:43 +0530

    Implemented MongoDB and Mongoose

--- Node JS/foodApp/app.js
+++ Node JS/foodApp/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const app = express();  //instance
+const mongoose = require("mongoose");
 
 app.use(express.json());  
 
@@ -31,7 +32,7 @@ let users = [
 
     userRouter
     .route("/")
-    .get(getUser)
+    .get(getUser)   //path specific middleware
     .post(postUser)
     .patch(updateUser)
     .delete(deleteUser)
@@ -42,7 +43,7 @@ let users = [
 
     authRouter
     .route("/signup")
-    .get(getSignUp)
+    .get(middleWare1,getSignUp,middleWare2)
     .post(postSignUp);
     
     
@@ -110,8 +111,10 @@ function getUserByID(req,res){
 }
 
 
-function getSignUp(req,res){
-    res.sendFile("public/index.html",{root : __dirname}); 
+function getSignUp(req,res,next){
+    console.log("getsignup function is called");
+    //res.sendFile("public/index.html",{root : __dirname}); 
+    next();
 }
 
 function postSignUp(req,res){
@@ -124,6 +127,73 @@ function postSignUp(req,res){
     
 
 }
+
+function middleWare1(req,res,next){
+
+    console.log("middleware1 encountered");
+    next();  //getsignup will be called
+
+}
+
+function middleWare2(req,res,next){
+
+    console.log("middlewar2 encountered");
+  
+     console.log("middleware 2 ended req/res cycle");
+    
+    res.sendFile("public/index.html",{root : __dirname}); 
+
+}
+
+const db_link = "mongodb+srv://admin:B6iHjfvHdcduaksq@anujkaushik.wywur.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+mongoose.connect(db_link)
+.then(function(db){
+    console.log("database connected");
+}).catch(function(err){
+    console.log(err);
+});
+
+const userSchema = mongoose.Schema({
+    name: {
+        type : String,
+        required : true,
+
+    },
+    email: {
+        type : String,
+        required : true,
+        unique : true
+    },
+    password: {
+        type : String,
+        required : true,
+        min : 7
+
+    },
+    confirmPassword: {
+        type : String,
+        required : true,
+        min : 7
+        
+    }
+});
+
+    // model =>
+
+const userModel = mongoose.model("userModel",userSchema);
+
+    (async function createUser(){
+    let users = {
+        name : "Anuj",
+        email : "anujkaushik1512@gmail.com",
+        password : "12345678",
+        confirmPassword : "12345678"
+    }
+    let data = await userModel.create(users);
+    console.log(data);
+
+})();
 
 
 
