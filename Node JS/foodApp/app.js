06bbd8dd602ec03f123f330ceae2b803f0ba2260
Anuj Kaushik <anujkaushik1512@gmail.com>
2021-11-08 13:13:54 +0530

    Encrypted the Password Using Hashing

--- Node JS/foodApp/app.js
+++ Node JS/foodApp/app.js
@@ -1,10 +1,9 @@
 const express = require("express");
 
 const app = express();  //instance
-const mongoose = require("mongoose");
-const emailValidator = require("email-validator");
 
-console.log('debugger hit');
+const userModel = require("./models/userModel");
+
 app.use(express.json());  
 
 app.listen(3000);
@@ -159,65 +158,6 @@ function middleWare2(req,res,next){
 
 }
 
-const db_link = "mongodb+srv://admin:B6iHjfvHdcduaksq@anujkaushik.wywur.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
-mongoose.connect(db_link)
-.then(function(db){
-    console.log("database connected");
-}).catch(function(err){
-    console.log(err);
-});
-
-
-
-const userSchema = mongoose.Schema({
-    name: {
-        type : String,
-        required : true,
-
-    },
-    email: {
-        type : String,
-        required : true,
-        unique : true,
-        validate : function(){
-            return emailValidator.validate(this.email);
-        }
-
-    },
-    password: {
-        type : String,
-        required : true,
-        min : 7
-
-    },
-    confirmPassword: {
-        type : String,
-        required : true,
-        min : 7,
-        validate : function(){  
-            return this.confirmPassword == this.password;    
-        }
-        
-    }
-});
-
-    // userSchema.pre('save',function(){
-    //     console.log("Before saving in database",this);
-    // });
-
-    // userSchema.post('save',function(doc){    //data that is saved in database is stored in doc
-    //     console.log("After saving in database",doc);
-    // });
-
-    userSchema.pre("save",function(){
-        this.confirmPassword = undefined;
-    });
-    
-
-    // model =>
-
-const userModel = mongoose.model("userModel",userSchema);
 
 //     (async function createUser(){
 //     let users = {
---
+++ Node JS/foodApp/models/userModel.js
@@ -0,0 +1,71 @@
+const mongoose = require("mongoose");
+const emailValidator = require("email-validator");
+const bcrypt = require("bcrypt");
+
+
+const db_link = "mongodb+srv://admin:B6iHjfvHdcduaksq@anujkaushik.wywur.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+mongoose.connect(db_link)
+.then(function(db){
+    console.log("database connected");
+}).catch(function(err){
+    console.log(err);
+});
+
+
+
+const userSchema = mongoose.Schema({
+    name: {
+        type : String,
+        required : true,
+
+    },
+    email: {
+        type : String,
+        required : true,
+        unique : true,
+        validate : function(){
+            return emailValidator.validate(this.email);
+        }
+
+    },
+    password: {
+        type : String,
+        required : true,
+        min : 7
+
+    },
+    confirmPassword: {
+        type : String,
+        required : true,
+        min : 7,
+        validate : function(){  
+            return this.confirmPassword == this.password;    
+        }
+        
+    }
+});
+
+    // userSchema.pre('save',function(){
+    //     console.log("Before saving in database",this);
+    // });
+
+    // userSchema.post('save',function(doc){    //data that is saved in database is stored in doc
+    //     console.log("After saving in database",doc);
+    // });
+
+    // userSchema.pre("save",function(){
+    //     this.confirmPassword = undefined;
+    // });
+
+    userSchema.pre("save",async function(){
+        let salt = await bcrypt.genSalt();
+        let hashedString = await bcrypt.hash(this.password,salt);
+        console.log(hashedString);
+    });
+    
+
+    // model =>
+
+const userModel = mongoose.model("userModel",userSchema);
+module.exports=userModel;
