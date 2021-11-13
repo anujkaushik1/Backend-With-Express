d03f5b4b58713ccd286e478711fc54804ac4221a
Anuj Kaushik <anujkaushik1512@gmail.com>
2021-11-13 10:45:32 +0530

    Attached Methods in Mongoose Schema

--- Node JS/foodApp/models/userModel.js
+++ Node JS/foodApp/models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const emailValidator = require("email-validator");
 const bcrypt = require("bcrypt");
+const crypto = require("crypto");
 
 
 const db_link = "mongodb+srv://admin:B6iHjfvHdcduaksq@anujkaushik.wywur.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
@@ -56,25 +57,17 @@ const userSchema = mongoose.Schema({
     resetToken : String
 });
 
-    // userSchema.pre('save',function(){
-    //     console.log("Before saving in database",this);
-    // });
+    userSchema.methods.createResetToken = function(){
+        const resetToken = crypto.randomBytes(32).toString("hex");
+        this.resetToken = resetToken;
+        return resetToken;
+    }
 
-    // userSchema.post('save',function(doc){    //data that is saved in database is stored in doc
-    //     console.log("After saving in database",doc);
-    // });
-
-    // userSchema.pre("save",function(){
-    //     this.confirmPassword = undefined;
-    // });
-
-    // userSchema.pre("save",async function(){
-    //     let salt = await bcrypt.genSalt();
-    //     let hashedString = await bcrypt.hash(this.password,salt);
-    //     this.password = hashedString;
-    //     console.log(hashedString);
-    // });
-    
+    userSchema.methods.resetPasswordHandler = function(password,confirmPassword){
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+        this.resetToken = undefined;   
+    }
 
     // model =>
 
