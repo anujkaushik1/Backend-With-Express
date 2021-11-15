f458d5cab5788fcc3e9651037579ec40f28a982a
Anuj Kaushik <anujkaushik1512@gmail.com>
2021-11-15 17:55:02 +0530

    Implemented Stripe API for Payments

---
+++ Node JS/foodApp/Router/bookingRouter.js
@@ -0,0 +1,12 @@
+
+const express = require("express");
+const bookingRouter = express.Router();
+const {protectRoute} = require("../controller/authController");
+const {createSession} = require("../controller/bookingController");
+bookingRouter.post("/createSession",protectRoute);
+bookingRouter.get('/createSession',function(req,res){
+    res.sendFile("C:/Users/ACER/Documents/Anuj/Backend Node JS/Node JS/foodApp/booking.html");
+});
+
+module.exports = bookingRouter;
+    
--- Node JS/foodApp/app.js
+++ Node JS/foodApp/app.js
@@ -9,11 +9,14 @@ app.listen(3000);
     const userRouter = require("./Router/userRouter");   //mini app
     const planRouter = require("./Router/planRouter"); 
     const reviewRouter = require("./Router/reviewRouter"); 
+const bookingRouter = require("./Router/bookingRouter");
+
      
 
     app.use("/users",userRouter);         //base route //router to use
     app.use("/plans",planRouter);
     app.use("/reviews",reviewRouter);
+    app.use("/booking",bookingRouter);
 
  
 
